1e0464affafa558ba4fddbc64beb7b3f96f2a1af
mochi-sann <moyurusuto@gmail.com>
2021-02-11 22:29:22 +0900

    aa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,12 @@
-import { Text, Box, Input, Container, Heading, Button } from "@chakra-ui/react";
+import {
+  Text,
+  Box,
+  Input,
+  Container,
+  Heading,
+  Button,
+  Link,
+} from "@chakra-ui/react";
 import { useState } from "react";
 // import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
 import Head from "next/head";
@@ -22,18 +30,19 @@ const Index = () => {
         <Text as="pre">
           {`#${TweetName}しか勝たん\n#${TweetName}すき\n#${TweetName}好き\n#${TweetName}とうとい\n#${TweetName}尊い\n#${TweetName}かわいい\n#${TweetName}可愛い\n#1日1回の${TweetName}ちゃんかわいい\n#1日n回の${TweetName}ちゃん可愛い\n#${TweetName}可愛すぎてキュン死\n`}
         </Text>
-        <Button
-          my="4"
-          as="a"
+        <Link
           href={
             "https://twitter.com/intent/tweet?text=" +
             encodeURIComponent(
               `#${TweetName}しか勝たん\n#${TweetName}すき\n#${TweetName}好き\n#${TweetName}とうとい\n#${TweetName}尊い\n#${TweetName}かわいい\n#${TweetName}可愛い\n#1日1回の${TweetName}ちゃんかわいい\n#1日n回の${TweetName}ちゃん可愛い\n#${TweetName}可愛すぎてキュン死\n`
             )
           }
+          isExternal
         >
-          ツイートする
-        </Button>
+          <Button my="4" as="a">
+            ツイートする
+          </Button>
+        </Link>
       </Box>
     </Container>
   );
