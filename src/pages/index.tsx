01c8e2b1270d243341243d72b2d2dc13ea4a9600
mochi-sann <moyurusuto@gmail.com>
2021-02-12 18:46:18 +0900

    間違えてindex.tsxを削除してしまっていたので戻した

---
+++ src/pages/index.tsx
@@ -0,0 +1,73 @@
+import {
+  Text,
+  Box,
+  Input,
+  Container,
+  Heading,
+  Button,
+  Link,
+} from "@chakra-ui/react";
+import { useState } from "react";
+// import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
+import Head from "next/head";
+// import { Container } from "../components/Container";
+
+const Index = () => {
+  const [TweetName, setTweetName] = useState("");
+
+  return (
+    <Container height="100vh">
+      <Head>
+        <title>かわいいハッシュタグツイート生成サイト</title>
+      </Head>
+      <Heading my="2">かわいいハッシュタグツイート生成サイト</Heading>
+      <Input
+        value={TweetName}
+        onChange={(e) => setTweetName(e.target.value)}
+        placeholder="ツイートする名前"
+      />
+      <Box m="4">
+        <Text as="pre">
+          {`#${TweetName}しか勝たん\n#${TweetName}すき\n#${TweetName}好き\n#${TweetName}とうとい\n#${TweetName}尊い\n#${TweetName}かわいい\n#${TweetName}可愛い\n#1日1回の${TweetName}ちゃんかわいい\n#1日n回の${TweetName}ちゃん可愛い\n#${TweetName}可愛すぎてキュン死\n`}
+        </Text>
+        <Link
+          href={
+            "https://twitter.com/intent/tweet?text=" +
+            encodeURIComponent(
+              `#${TweetName}しか勝たん\n#${TweetName}すき\n#${TweetName}好き\n#${TweetName}とうとい\n#${TweetName}尊い\n#${TweetName}かわいい\n#${TweetName}可愛い\n#1日1回の${TweetName}ちゃんかわいい\n#1日n回の${TweetName}ちゃん可愛い\n#${TweetName}可愛すぎてキュン死\n\nhttps://kawaii-tweet.vercel.app`
+            )
+          }
+          isExternal
+        >
+          <Button my="4" as="a">
+            ツイートする
+          </Button>
+        </Link>
+      </Box>
+      <Link
+        href="https://github.com/mochi-sann/kawaiihashutagtweet"
+        color="blue.500"
+        mr="2"
+      >
+        Githubのリポジトリ
+      </Link>
+      <Link
+        href="https://github.com/DevelopersPackages-org/KawaiiHashTweetGenerator"
+        color="blue.500"
+      >
+        移植元のGithubのリポジトリ
+      </Link>{" "}
+      <br />
+      Copyright (c) 2021 Haruka Sato, mochi <br />
+      Forked: Copyright (c) 2021 Haruka Sato <br />
+      under{" "}
+      <Link
+        href="https://github.com/mochi-sann/kawaiihashutagtweet/blob/main/LICENSE"
+        color="blue.500"
+      >
+        Mit+3 License
+      </Link>
+    </Container>
+  );
+};
+export default Index;
